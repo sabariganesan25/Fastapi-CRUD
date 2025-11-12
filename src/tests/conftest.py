a019fac4c70c2d0de694e91301dec2ef6751cecf
sabariganesan25 <sabariganesan257@gmail.com>
2025-11-12 14:14:53 +0530

    Commits The Learing Progress

---
+++ src/tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from starlette.testclient import TestClient
+
+from app.main import app
+
+
+@pytest.fixture(scope="module")
+def test_app():
+    client = TestClient(app)
+    yield client  # testing happens here
+def test_ping(test_app):
+    response = test_app.get("/ping")
+    assert response.status_code == 200
+    assert response.json() == {"ping": "pong!"} 
